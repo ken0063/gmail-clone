d8a19d883f9bb88aff19dacfa84134f9f890e2f7
Kenneth Olukotun <kenneth.olukotun@indicina.co>
2021-10-24 05:03:05 +0100

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import tw from 'twin.macro';
+
+const AppContainer = tw.div`m-0 p-0 w-screen h-screen`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AppContainer>hello world</AppContainer>;
 }
 
 export default App;
